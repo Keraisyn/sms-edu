04e4c9758fa764d0b8b3b1397480c7f67761dcc6
Keraisyn <matthewsma21@gmail.com>
2020-04-26 10:41:01 -0400

    API change

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACa24dbdea10c742297100c3269f8f777c'
-auth_token = 'b906b3924ea39ae373f1b315ad969376'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
